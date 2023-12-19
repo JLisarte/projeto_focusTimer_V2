247bc8797ec50720cfbfb5772c1fac41146d9525
JLisarte <juliano.lisarte@gmail.com>
2023-12-19 18:04:40 -0300

    Add files

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,15 @@
+export const buttonTree = new Audio("./assets/sounds/floresta.wav")
+buttonTree.loop = true
+
+export const buttonRain = new Audio("./assets/sounds/chuva.wav")
+buttonRain.loop = true
+
+export const buttonFire = new Audio("./assets/sounds/lareira.wav")
+buttonFire.loop = true
+
+export const buttonCoffe = new Audio("./assets/sounds/cafeteria.wav")
+buttonCoffe.loop = true
+
+export const buttonPressAudio = new Audio("./assets/sounds/button-press.wav")
+
+export const kitchenTimer = new Audio("./assets/sounds/kichen-timer.mp3")
